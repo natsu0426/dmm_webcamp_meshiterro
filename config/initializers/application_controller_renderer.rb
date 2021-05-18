a019f3f5e3a4b014e73810e7f84efa13864c287c
natsu0426 <natsu0426coffee@gmail.com>
2021-05-18 05:43:11 +0000

    完成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '8ed8b69840e89fa20b38f8a9e93a207bc6574dc4def04db6dde475c00fc3d07acb7478e4ecde92102f26eb3aa3a8a72c3037a5355a8e9e497da11290fc15d4a9'
